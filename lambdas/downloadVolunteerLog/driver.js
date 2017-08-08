c2a8eb853caa2620d09a8669ea3902e5cc1ebb57
CJ Martone <cjm74@njit.edu>
2017-08-08 08:31:00 -0400

    Added downloadSignnInLog.

--- lambdas/downloadVolunteerLog/driver.js
+++ lambdas/downloadVolunteerLog/driver.js
@@ -3,3 +3,7 @@ let lambdas = require('./index');
 lambdas.downloadVolunteerLog({}, {}, (something, response) => {
   console.log(response);
 });
+
+lambdas.downloadSignInLog({}, {}, (something, response) => {
+  console.log(response);
+});
